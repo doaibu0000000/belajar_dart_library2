822db0b7a0c575ce9c60fee190a37c16212ec718
Doa Ibu <doa.ibu0000000@gmail.com>
2023-06-13 14:02:04 +0700

    belajar dart library2

---
+++ example/belajar_dart_library2_example.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_dart_library2/belajar_dart_library2.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/belajar_dart_library2.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library belajar_dart_library2;
+
+export 'src/belajar_dart_library2_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
